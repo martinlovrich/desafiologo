99bace9db60b4d43e188a16fe8d487327185a44e
martinlovrich <71386978+martinlovrich@users.noreply.github.com>
2021-02-03 18:23:02 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavbarComponent from './components/navbar/';
+import ItemListContainer from './containers/ItemListContainer';
+
+
+const App = () => {
+  return (
+    <>
+    <NavbarComponent />
+    
+    <ItemListContainer greeting={"Listado de articulos"}/>
+
+    </>
+  );
+}
+
+export default App;
